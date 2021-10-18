5006e320ba2114dcdacd6d1c3550a0d3b1fdf4e3
MaxKoval1ov <maxkov112211@gmail.com>
2021-10-18 23:35:57 +0300

    add:BST solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,135 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
+* Implement simple binary search node according to task description
 * using Node from extensions
 */
-module.exports = class BinarySearchTree {
+module.exports = class BinarySearcTree {
+    constructor() {
+        this.node = null;
+    }
+    root() {
+        return this.node;
+    }
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    add(data) {
+        this.node = innerAdd(this.node, data);
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        function innerAdd(node, data) {
+            if (!node) {
+                return new Node(data);
+            }
+            if (node.data === data) {
+                return node;
+            }
+            if (data < node.data) {
+                node.left = innerAdd(node.left, data);
+            } else {
+                node.right = innerAdd(node.right, data);
+            }
+            return node;
+        }
+    }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    has(data) {
+        return innerHas(this.node, data);
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        function innerHas(node, data) {
+            if (!node) {
+                return false;
+            }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            if (node.data === data) {
+                return true;
+            }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            return data < node.data ?
+                innerHas(node.left, data) :
+                innerHas(node.right, data);
+        }
+    }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    find(data) {
+        let node = this.node;
+        while (node) {
+            if (data > node.data) {
+                node = node.right;
+            } else if (data < node.data) {
+                node = node.left;
+            } else if (data == node.data) {
+                return node;
+            }
+        }
+        return null;
+    }
 
+    remove(data) {
+        this.node = removeNode(this.node, data);
+
+        function removeNode(node, data) {
+            if (!node) {
+                return null;
+            }
+
+            if (data < node.data) {
+                node.left = removeNode(node.left, data);
+                return node;
+            } else if (node.data < data) {
+                node.right = removeNode(node.right, data);
+                return node;
+            } else {
+                if (!node.left && !node.right) {
+                    return null;
+                }
+
+                if (!node.left) {
+                    node = node.right;
+                    return node;
+                }
+
+                if (!node.right) {
+                    node = node.left;
+                    return node;
+                }
+
+                let minRight = node.right;
+                while (minRight.left) {
+                    minRight = minRight.left;
+                }
+                node.data = minRight.data;
+
+                node.right = removeNode(node.right, minRight.data);
+
+                return node;
+            }
+        }
+    }
+
+    min() {
+        if (!this.node) {
+            return;
+        }
+
+        let node = this.node;
+        while (node.left) {
+            node = node.left;
+        }
+
+        return node.data;
+    }
+
+    max() {
+        if (!this.node) {
+            return;
+        }
+
+        let node = this.node;
+        while (node.right) {
+            node = node.right;
+        }
+
+        return node.data;
+    }
 }
